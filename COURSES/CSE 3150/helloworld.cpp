03526404da407781b69e8389d946a61a967e5f56
Riley Francis <rileyfrancis01@gmail.com>
2026-01-20 16:05:19 -0500

    update from PC

---
+++ COURSES/CSE 3150/helloworld.cpp
@@ -0,0 +1,14 @@
+// This is a comment
+
+/*
+This is a multi-line comment
+*/
+
+// This is an include directive
+#include <iostream>
+
+int main() {
+    // Print "Hello, World!" to the console
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
