eaa68cd21d3c649816d1abc6a315d81f87702fa0
Riley Francis <rileyfrancis01@gmail.com>
2026-01-20 16:07:33 -0500

    vault backup: 2026-01-20 16:07:33

--- COURSES/CSE 3150/helloworld.cpp
+++ COURSES/CSE 3150/helloworld.cpp
@@ -10,5 +10,12 @@ This is a multi-line comment
 int main() {
     // Print "Hello, World!" to the console
     std::cout << "Hello, World!" << std::endl;
+
+    // We can declare a new variable
+    int num_students;
+    num_students = 100;
+
+    std::cout << "Number of students: " << num_students << std::endl;
+
     return 0;
 }
