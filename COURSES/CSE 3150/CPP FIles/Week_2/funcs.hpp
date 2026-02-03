271fbece18c4773c5da00e910e416a9f6aef1463
Riley Francis <rileyfrancis01@gmail.com>
2026-02-03 15:52:09 -0500

    update from PC

---
+++ COURSES/CSE 3150/CPP FIles/Week_2/funcs.hpp
@@ -0,0 +1,3 @@
+// You can then create a header file (this one) with the hpp extension that includes function headers
+
+void example()
---
+++ COURSES/CSE 3150/CPP FIles/Week_2/week2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+// The pound include statement at the top here says "take the contents of this package and use it for linking"
+
+// Functions must be declared before you call them. If you created this function after main(), it would error.
+void my_function(int x) {
+    std::cout << x << std::endl;
+}
+
+// You can also declare just a function stub and then define it later
+void my_function2(int x);
+
+int main() {
+    int y;
+    y = 50;
+    my_function(y);
+    return 0;
+}
+
+// Now we define it later after it was declared earlier
+void my_function2(int x) {
+    std::cout << x << std::endl;
+}
