87d44f49f855777150c293d68d4437d1ca6c1600
Riley Francis <rileyfrancis01@gmail.com>
2026-02-05 16:13:17 -0500

    vault backup: 2026-02-05 16:13:17
    Affected files:
    .obsidian/workspace.json
    COURSES/CSE 3150/CPP/Week_3_p2/build/program.out
    COURSES/CSE 3150/CPP/Week_3_p2/src/.example.cpp.swp
    COURSES/CSE 3150/CPP/Week_3_p2/src/example.cpp
    COURSES/CSE 3150/CPP/program.out

--- COURSES/CSE 3150/CPP/Week_3_p2/src/example.cpp
+++ COURSES/CSE 3150/CPP/Week_3_p2/src/example.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <string>
 
-using std::cout, std::endl, std::string;
+using std::cout, std::cin, std::endl, std::string;
 
 int main() {
     int age;
     string name;
 
-    cout << "Enter your age ";
-    std::cin >> age;
-    std::cin.ignore();
+    cout << "Enter your age and name ";
+    cin >> age;
+    cin.ignore();
 
     std::getline(std::cin, name);
     cout << "You entered " << name << age << endl;
