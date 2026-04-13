8096d195889b24c2cf1cee1bb33092539c559cf0
Riley Francis <rileyfrancis01@gmail.com>
2026-04-13 14:34:18 -0400

    vault backup: 2026-04-13 14:34:18
    Affected files:
    .obsidian/workspace.json
    COURSES/CSE 3150/Labs/day_18_lab/.vscode/c_cpp_properties.json
    COURSES/CSE 3150/Labs/day_18_lab/include/vector.hpp
    COURSES/CSE 3150/Labs/day_18_lab/src/main.cpp

---
+++ COURSES/CSE 3150/Labs/day_18_lab/include/vector.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <cstddef> // for size_t
+#include <stdexcept>
+
+
+template <typename T>
+class Vector {
+private:
+    T* data_;
+    size_t size_;
+    size_t capacity_;
+    
+    void resize_capacity(size_t new_capacity);
+
+public:
+    Vector();
+    ~Vector();
+
+    size_t size() const;
+    size_t capacity() const;
+    bool empty() const;
+
+    void push_back(const T& value);
+
+    T& at(size_t index);
+};
